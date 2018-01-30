49f48d43abba95ccb2c3683bfacc0dc6e07c3769
mehow <mgluza@gmail.com>
2018-01-30 19:20:36 +0100

    Initial commit

---
+++ BasicInheritance/BasicInheritance/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Car
+    {
+        public readonly int maxSpeed;
+        private int currSpeed;
+
+        public Car(int max)
+        {
+            this.maxSpeed = max;
+        }
+        public Car()
+        {
+            maxSpeed = 55;
+        }
+        public int Speed
+        {
+            get { return currSpeed; }
+            set
+            {
+                currSpeed = value;
+                if (currSpeed > maxSpeed)
+                {
+                    currSpeed = maxSpeed;
+                }
+            }
+        }
+    }
+
+    class MiniVan : Car
+    {
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("***** Basic Inheritance *****\n");
+            //Car myCar = new Car(80);
+
+            //myCar.Speed = 50;
+            //Console.WriteLine($"My car is going {myCar.Speed} MPH");
+
+            MiniVan myVan = new MiniVan();
+            myVan.Speed = 110;
+            Console.WriteLine($"My van is going {myVan.Speed} MPH");
+            Console.ReadLine();
+    }
+    }
+}
---
+++ EmployeeApp/EmployeeApp/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                else empName = value;
+            }
+        }
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+        public Employee() { };
+        public Employee(string name, int id, float pay)
+        : this(name, 0, id, pay) { }
+        public Employee(string name, int age, int id, float pay)
+        {
+            Name = name;
+            Age = age;
+            ID = id;
+            Pay = pay;
+        }
+
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine($"Name: {empName}");
+            Console.WriteLine($"ID: {empID}");
+            Console.WriteLine($"Pay: {currPay}");
+            Console.WriteLine($"Age: {empAge}");
+        }
+    }
+}
